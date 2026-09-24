26dc2f560f7c6cb52110423a9c13da193c1bd544
Julia <133766282+RudykhYuliya@users.noreply.github.com>
2026-09-24 16:02:12 +0300

    Правки по комментариям ревьюера

--- tests/test_order_feed.py
+++ tests/test_order_feed.py
@@ -10,6 +10,18 @@ class TestOrderFeed:
         number = feed_page.open_first_order()
         assert number.startswith('#')
 
+    @allure.title('Заказ отображается в истории заказов')
+    def test_order_is_in_history(self, user, main_page, login_page, account_page):
+        main_page.click_account()
+        login_page.login(user['email'], user['password'])
+        main_page.add_ingredient(data.BUN)
+        main_page.add_ingredient(data.SAUCE)
+        number = main_page.place_order()
+        main_page.close_modal()
+        main_page.click_account()
+        account_page.open_order_history()
+        assert account_page.is_order_in_history(number)
+
     @allure.title('Заказ из истории отображается в ленте заказов')
     def test_user_order_is_in_feed(self, user, main_page, login_page, account_page, feed_page):
         main_page.click_account()
@@ -20,7 +32,6 @@ class TestOrderFeed:
         main_page.close_modal()
         main_page.click_account()
         account_page.open_order_history()
-        assert account_page.is_order_in_history(number)
         feed_page.open()
         assert feed_page.is_order_in_feed(number)
 
